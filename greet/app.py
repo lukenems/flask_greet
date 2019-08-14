3fcbfce4f8f124068cfdf31be67ccd53a619cf8e
Luke Nemy <lukenemy7@gmail.com>
2019-08-14 11:48:52 -0700

    first greet app routes created and running

---
+++ greet/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/welcome')
+def welcome_greeting():
+    'Displays Welcome html'
+    return "<html><body><h1>Welcome!</h1></body></html>"
+
+@app.route('/welcome/home')
+def home_greeting():
+    'Displays welcome home html'
+    return "<html><body><h1>Welcome Home!</h1></body></html>"
+
+@app.route('/welcome/back')
+def back_greeting():
+    'Displays welcome back html'
+    return "<html><body><h1>Welcome back!</h1></body></html>"
